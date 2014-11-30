61f4fd7d441aaf75018a274683ecc973ae5f313c
satori99 <satori.developer@gmail.com>
2014-12-01 09:22:21 +1100

    Started new addon UI

--- scripts/addons/io_mesh_threejs_object/__init__.py
+++ scripts/addons/io_mesh_threejs_object/__init__.py
@@ -92,30 +92,150 @@ class ThreeObjectExportOperator(Operator, ExportHelper):
         options={"SKIP_SAVE"}
         )
 
-    def invoke(self, context, event):
+    # object options
 
-        if not self.filepath:
+    object_types = EnumProperty(
+        name="Object Types",
+        description="Export object types.",
+        items=(("MESH", "Mesh", "", "", 1),      # OUTLINER_OB_MESH
+               ("CURVE", "Curve", "", "", 2),    # OUTLINER_OB_CURVE
+               ("LAMP", "Lamp", "", "", 4),      # OUTLINER_OB_LAMP
+               ("CAMERA", "Camera", "", "", 8),  # OUTLINER_OB_CAMERA
+               ("EMPTY", "Empty", "", "", 16),   # OUTLINER_OB_EMPTY
+               ),
+        default={"MESH"},
+        options={"ENUM_FLAG"}
+        )
 
-            blend_filepath = context.blend_data.filepath
+    uniform_scale = FloatProperty(
+        name="Uniform Scale",
+        description="Apply uniform scale to all exported objects.",
+        default=1.0,
+        min=0.01,
+        max=100,
+        )
 
-            if blend_filepath:
+    selected_only = BoolProperty(
+        name="Selected Only",
+        description="Export selected scene objects only.",
+        default=False
+        )
 
-                self.filepath = display_name_from_filepath(blend_filepath)
+    include_descendents = BoolProperty(
+        name="Include Descendents",
+        description="Always export descendents of selected objects,"
+                    "even when they are not explicitly selected themselves.",
+        default=False
+        )
 
-            else:
+    # mesh options
 
-                self.filepath = "untitled"
+    apply_mesh_modifiers = BoolProperty(
+        name="Apply Mesh Modifiers",
+        description="Apply mesh modifiers to exported geometry",
+        default=True
+        )
 
-        context.window_manager.fileselect_add(self)
+    mesh_modifier_mode = EnumProperty(
+        name="Mesh Modifier Mode",
+        description="Modifier mode type",
+        items=(("VIEW", "View", "", "RESTRICT_VIEW_OFF", 1),
+               ("RENDER", "Render", "", "RESTRICT_RENDER_OFF", 2)),
+        default="RENDER"
+        )
 
-        return {"RUNNING_MODAL"}
+    split_by_material = BoolProperty(
+        name="Split by Material",
+        description="Split exported geometry by material",
+        default=True
+        )
+
+    # geometry options
+
+    export_normals = BoolProperty(
+        name="Normal",
+        description="Export BufferGeometry normal attribute",
+        default=True
+        )
+
+    export_uvs = BoolProperty(
+        name="UV",
+        description="Export BufferGeometry uv attributes",
+        default=True
+        )
+
+    export_colors = BoolProperty(
+        name="Color",
+        description="Export BufferGeometry color attribute",
+        default=True
+        )
+
+    export_index = BoolProperty(
+        name="Index",
+        description="Export BufferGeometry index attribute",
+        default=True
+        )
+
+    # material options
+
+    export_materials = BoolProperty(
+        name="Export Materials",
+        description="Export assigned mesh materials",
+        default=True
+        )
+
+    #
 
     def draw(self, context):
         '''
         Draw the export options
         '''
         layout = self.layout
-        layout.separator()
+
+        # Object Options
+
+        layout.label("Object Options:", icon="OBJECT_DATA")
+        row = layout.row()
+        col = row.column()
+        col.prop(self.properties, "selected_only")
+        col = row.column()
+        col.prop(self.properties, "include_descendents")
+        col.enabled = self.selected_only
+        layout.prop(self.properties, "uniform_scale")
+        layout.prop(self, "object_types")
+
+        # Mesh Options
+
+        box = layout.box()
+        box.label("Mesh Options:", icon="OUTLINER_OB_MESH")
+        row = box.row()
+        col = row.column()
+        col.prop(self.properties, "apply_mesh_modifiers")
+        col = row.column()
+        col.prop(self.properties, "mesh_modifier_mode", text="")
+        col.enabled = self.apply_mesh_modifiers
+        row = box.row()
+        col = row.column()
+        col.prop(self.properties, "split_by_material")
+        box.enabled = "MESH" in self.object_types
+
+        # BufferGeometry Attributes
+
+        box = layout.box()
+        box.label("BufferGeometry Attributes:", icon="OUTLINER_DATA_MESH")
+        row = box.row()
+        row.prop(self.properties, "export_normals")
+        row.prop(self.properties, "export_uvs")
+        row = box.row()
+        row.prop(self.properties, "export_colors")
+        row.prop(self.properties, "export_index")
+        box.enabled = any(i in self.object_types for i in ("MESH", "CURVE"))
+
+        # Material Options
+
+        box = layout.box()
+        box.label("Materials:", icon="MATERIAL")
+        box.prop(self.properties, "export_materials")
 
     def execute(self, context):
         '''
@@ -123,15 +243,13 @@ class ThreeObjectExportOperator(Operator, ExportHelper):
         '''
         try:
 
-            self.filepath = ensure_ext(self.filepath, self.filename_ext)
+            # self.filepath = ensure_ext(self.filepath, self.filename_ext)
 
-            keywords = self.as_keywords(ignore=("", ))
+            # keywords = self.as_keywords(ignore=("", ))
 
-            # from . import object
+            from .export import export
 
-            # return object.export(context, **keywords)
-
-            return {"FINISHED"}
+            return export(context, **self.as_keywords())
 
         except:
 
